d729eeed665c5bb904a5a0747704ff591fa14a48
srikutty <sri9132@gmail.com>
2020-10-27 11:11:23 +0530

    completed

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,17 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::namespace('Api')->group(function() {
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::prefix('auth')->group(function(){
+
+        Route::post('login','AuthController@login');
+        Route::post('signup','AuthController@signup');
+    });
+    Route::group([
+        'middleware'=>'auth:api'
+    ], function(){
+       Route::get('helloworld','AuthController@index');
+       Route::post('logout', 'AuthController@logout');
+    });
 });
